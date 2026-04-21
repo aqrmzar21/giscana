5c32b3e1ba28590fb433e158ddfde5492ecf0132
aqrmzar21 <aqramzoombar@gmail.com>
2026-04-21 09:21:38 +0800

    membedakan peta dashboard

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -30,7 +30,7 @@
             <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
                 <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
                     <div>
-                        <h3 class="text-lg font-semibold mb-4">Gisnater</h3>
+                        <h3 class="text-lg font-semibold mb-4">Giscana</h3>
                         <p class="text-gray-400 mb-4">
                             Geographic Information System for Natural Disaster  
                             di Kawasan Pesisir Bone, Kabupaten Bone Bolango, Provinsi Gorontalo.
