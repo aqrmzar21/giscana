056ed2e588ee13df2d61b34d948afe96cd049ecf
aqrmzar21 <aqramzoombar@gmail.com>
2026-05-22 01:16:38 +0800

    update garis batas desa warna putih

--- resources/views/map/partials/map-scripts.blade.php
+++ resources/views/map/partials/map-scripts.blade.php
@@ -273,7 +273,7 @@
                             .then(data => {
                                 L.geoJSON(data, {
                                     style: {
-                                        color: '#000000',
+                                        color: '#ffffffff',
                                         weight: 1,
                                         fillColor: '#60a5fa',
                                         fillOpacity: 0.1,
