09a481bc2f9e0c6e561b6179e5c6c4ee7561d7a9
aqrmzar21 <aqramzoombar@gmail.com>
2026-09-11 14:28:53 +0800

    FIX PANEL MOBILE

--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -9,6 +9,10 @@
     <!-- Peta Utama -->
     <div id="map" class="absolute inset-0 h-screen w-screen z-0"></div>
 
+    <div class="absolute md:bottom-4 bottom-10 left-4 z-50 md:p-2 pb-6">
+        @include('map.partials.map-legend-content')
+    </div>
+    
     <!-- 🔝 Header Bar Melayang (Mobile & Desktop) -->
     <div class="fixed top-4 left-4 right-4 z-40 flex items-center justify-between bg-white/95 backdrop-blur-md px-4 py-2.5 rounded-2xl shadow-md border border-slate-200/80 md:w-auto md:right-auto">
         <div class="flex items-center gap-3">
